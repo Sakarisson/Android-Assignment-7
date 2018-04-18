0d383a33dd75f17d22ead2d78cce8316718a689a
Kristian Sakarisson <kristian@sakarisson.com>
2018-04-18 20:07:11 +0300

    Added project files as well as basic test layout file

---
+++ app/src/main/java/com/sakarisson/kristian/androidassignment4/DialpadView.java
@@ -0,0 +1,11 @@
+package com.sakarisson.kristian.androidassignment4;
+
+import android.content.Context;
+import android.support.constraint.ConstraintLayout;
+import android.util.AttributeSet;
+
+public class DialpadView extends ConstraintLayout {
+    public DialpadView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
