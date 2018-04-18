ff7be23c99479e89554c741518aa3b972991585e
Kristian Sakarisson <kristian@sakarisson.com>
2018-04-18 21:04:12 +0300

    Added click listeners

--- app/src/main/java/com/sakarisson/kristian/androidassignment4/MainActivity.java
+++ app/src/main/java/com/sakarisson/kristian/androidassignment4/MainActivity.java
@@ -2,12 +2,46 @@ package com.sakarisson.kristian.androidassignment4;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.test);
+        initializeListeners();
+    }
+
+    private void initializeListeners() {
+        ImageView[] buttons = {
+                findViewById(R.id.imageView0),
+                findViewById(R.id.imageView1),
+                findViewById(R.id.imageView2),
+                findViewById(R.id.imageView3),
+                findViewById(R.id.imageView4),
+                findViewById(R.id.imageView5),
+                findViewById(R.id.imageView6),
+                findViewById(R.id.imageView7),
+                findViewById(R.id.imageView8),
+                findViewById(R.id.imageView9),
+                findViewById(R.id.imageViewStar),
+                findViewById(R.id.imageViewPound),
+        };
+        for (ImageView button : buttons) {
+            initializeIndividualListener(button);
+        }
+    }
+
+    private void initializeIndividualListener(ImageView button) {
+        View.OnClickListener buttonListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Do something with sound or other
+            }
+        };
+        button.setOnClickListener(buttonListener);
     }
 }
