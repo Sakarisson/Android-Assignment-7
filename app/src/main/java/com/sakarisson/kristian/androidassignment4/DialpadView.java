1b33eaa7cafbc160ad8a8ec8b62f019c1aba4acc
Kristian Sakarisson <kristian@sakarisson.com>
2018-04-21 17:04:35 +0300

    Added keyboard input handling

--- app/src/main/java/com/sakarisson/kristian/androidassignment4/DialpadView.java
+++ app/src/main/java/com/sakarisson/kristian/androidassignment4/DialpadView.java
@@ -2,26 +2,34 @@ package com.sakarisson.kristian.androidassignment4;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.media.MediaPlayer;
 import android.util.AttributeSet;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TableLayout;
 
+import java.security.Key;
+
 public class DialpadView extends TableLayout {
+    private Context context;
+
+    ImageView[] buttons;
+    int[] rawButtonSounds;
+
     public DialpadView(Context context, AttributeSet attrs) {
         super(context, attrs);
         init();
-
     }
 
     private void init() {
+        context = getContext();
         inflate(getContext(), R.layout.dialpad_layout, this);
         initializeListeners();
     }
 
     private void initializeListeners() {
-        ImageView[] buttons = {
-                findViewById(R.id.imageView0),
+        buttons = new ImageView[] {
                 findViewById(R.id.imageView1),
                 findViewById(R.id.imageView2),
                 findViewById(R.id.imageView3),
@@ -32,35 +40,95 @@ public class DialpadView extends TableLayout {
                 findViewById(R.id.imageView8),
                 findViewById(R.id.imageView9),
                 findViewById(R.id.imageViewStar),
+                findViewById(R.id.imageView0),
                 findViewById(R.id.imageViewPound),
         };
-        for (ImageView button : buttons) {
-            initializeIndividualListener(button);
+        rawButtonSounds = new int[] {
+                R.raw.one,
+                R.raw.two,
+                R.raw.three,
+                R.raw.four,
+                R.raw.five,
+                R.raw.six,
+                R.raw.seven,
+                R.raw.eight,
+                R.raw.nine,
+                R.raw.star,
+                R.raw.zero,
+                R.raw.pound,
+        };
+        for (int i = 0; i < buttons.length; i++) {
+            initializeIndividualListener(i);
         }
     }
 
-    private void initializeIndividualListener(final ImageView button) {
+    private void initializeIndividualListener(final int index) {
         View.OnClickListener buttonListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                buttonWasClicked(button);
+                buttonWasClicked(index);
             }
         };
-        button.setOnClickListener(buttonListener);
+        buttons[index].setOnClickListener(buttonListener);
     }
 
-    private void buttonWasClicked(final ImageView button) {
+    private void buttonWasClicked(final int index) {
+        final MediaPlayer buttonSound = MediaPlayer.create(context, rawButtonSounds[index]);
         // Run new thread for color switcharoo
         new Thread(new Runnable() {
             public void run() {
                 try {
-                    button.setBackgroundColor(Color.rgb(100, 100, 100));
+                    buttonSound.start();
+                    buttons[index].setBackgroundColor(Color.rgb(100, 100, 100));
                     Thread.sleep(150);
-                    button.setBackgroundColor(Color.rgb(255, 255, 255));
+                    buttons[index].setBackgroundColor(Color.rgb(255, 255, 255));
                 } catch (Exception e) {
                     System.out.println(e);
                 }
             }
         }).start();
+    }
+
+    public void keyWasPressed(int keyCode) {
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_1:
+                buttonWasClicked(0);
+                return;
+            case KeyEvent.KEYCODE_2:
+                buttonWasClicked(1);
+                return;
+            case KeyEvent.KEYCODE_3:
+                buttonWasClicked(2);
+                return;
+            case KeyEvent.KEYCODE_4:
+                buttonWasClicked(3);
+                return;
+            case KeyEvent.KEYCODE_5:
+                buttonWasClicked(4);
+                return;
+            case KeyEvent.KEYCODE_6:
+                buttonWasClicked(5);
+                return;
+            case KeyEvent.KEYCODE_7:
+                buttonWasClicked(6);
+                return;
+            case KeyEvent.KEYCODE_8:
+                buttonWasClicked(7);
+                return;
+            case KeyEvent.KEYCODE_9:
+                buttonWasClicked(8);
+                return;
+            case KeyEvent.KEYCODE_STAR:
+                buttonWasClicked(9);
+                return;
+            case KeyEvent.KEYCODE_0:
+                buttonWasClicked(10);
+                return;
+            case KeyEvent.KEYCODE_POUND:
+                buttonWasClicked(11);
+                return;
+            default:
+                return;
+        }
     }
 }
--- app/src/main/java/com/sakarisson/kristian/androidassignment4/MainActivity.java
+++ app/src/main/java/com/sakarisson/kristian/androidassignment4/MainActivity.java
@@ -2,14 +2,23 @@ package com.sakarisson.kristian.androidassignment4;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DialpadView dialpadView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        DialpadView dialpadView = findViewById(R.id.dialpadView);
+        dialpadView = findViewById(R.id.dialpadView);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        dialpadView.keyWasPressed(keyCode);
+        return true;
     }
 }
