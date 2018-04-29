7e50fd4e31ead3c5c68820a2e62c9f5105774177
Kristian Sakarisson <kristian@sakarisson.com>
2018-04-29 17:55:05 +0300

    Added simple toolbar button

--- app/src/main/java/com/sakarisson/kristian/androidassignment5/MainActivity.java
+++ app/src/main/java/com/sakarisson/kristian/androidassignment5/MainActivity.java
@@ -1,14 +1,16 @@
 package com.sakarisson.kristian.androidassignment5;
 
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
     private DialpadView dialpadView;
-    ActionBar actionBar;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +18,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         dialpadView = findViewById(R.id.dialpadView);
-        actionBar = getSupportActionBar();
+        toolbar = findViewById(R.id.toolbar_menu);
+        setSupportActionBar(toolbar);
     }
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         dialpadView.keyWasPressed(keyCode);
         return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        return super.onOptionsItemSelected(item);
     }
 }
